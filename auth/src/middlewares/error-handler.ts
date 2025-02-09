6a40f157a3a67bbb85aa7dfda864342520204944
Thiago <martinsmthiago@gmail.com>
2025-02-09 20:16:31 -0300

    add error handlers

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,17 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
+import { CustomError } from '../errors/custom-error';
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction ) => {
-    console.error(err.stack);
-    res.status(400).send('Something broke!');
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+
+  res.status(400).send({
+    errors: [{ message: 'Something went wrong' }],
+  });
 };

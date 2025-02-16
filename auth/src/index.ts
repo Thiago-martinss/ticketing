6f9077066f78cd35218bd8fb82678ed7b49244fa
Thiago <martinsmthiago@gmail.com>
2025-02-16 12:57:21 -0300

    add user authentication and validation

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -17,33 +17,36 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: true, // Set to true in production
-    httpOnly: true, // Set to true in production
+    secure: true,
   })
-)
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.get('*', async (req, res) => {
+app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
 
-app.use(errorHandler); // Must be the last middleware in the chain
+app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
   try {
-    await mongoose.connect('mongodb:///auth-mongo-srv:27017/auth');
-    console.log('Connected to the database');
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+
+    console.log('Connected to MongoDb');
   } catch (err) {
-    console.error('Error connecting to the database', err);
-    process.exit(1);
+    console.error(err);
   }
 
   app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Listening on port 3000!');
   });
 };
 

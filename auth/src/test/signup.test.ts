63d20d0bf854ec0e1a533704baaf466dc5b87840
Thiago <martinsmthiago@gmail.com>
2025-02-17 06:21:54 -0300

    add tests

---
+++ auth/src/test/signup.test.ts
@@ -0,0 +1,62 @@
+import request from 'supertest';
+import { app } from '../app';
+
+it('returns a 201 on successful signup', () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password123',
+    })
+    .expect(201);
+});
+
+it('returns a 400 with an error message if the email is invalid', () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test.test',
+      password: 'password123',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an error message if the password is too short', () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'pas',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({email: 'test@test.com'})
+    .expect(400);
+
+    await request(app)
+    .post('/api/users/signup')
+    .send({password: "password123"})
+    .expect(400);
+});
+
+it('disallows duplicate email addresses', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password123',
+    })
+    .expect(201);
+
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'differentpassword',
+    })
+    .expect(400);
+  });

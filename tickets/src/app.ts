16f4c2cbeff1a79a9d75a7f15d441cf026f988df
Thiago <martinsmthiago@gmail.com>
2025-03-24 16:33:33 -0300

    add show.ts

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -8,6 +8,7 @@ import {
 } from "@tmatta-tickets/common";
 import cookieSession from "cookie-session";
 import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
 
 const app = express();
 app.set("trust proxy", true);
@@ -21,6 +22,7 @@ app.use(
 
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();

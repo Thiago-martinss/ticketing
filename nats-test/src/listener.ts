5bf3000f5a8b62fcb8aa085a3edc52875f9ceae8
Thiago <martinsmthiago@gmail.com>
2025-04-28 20:02:11 -0300

    add group queue and randombytes

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,18 +1,19 @@
-import nats from 'node-nats-streaming';
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
 
 console.clear();
 
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
-  const subscription = stan.subscribe('ticket:created');
+  const subscription = stan.subscribe('ticket:created', 'queue-group-name');
 
-  subscription.on('message', (msg) => {
-    console.log('Message received');
+  subscription.on('message', (msg: Message) => {
     const data = msg.getData();
     console.log(data.toString('utf-8'));
   })

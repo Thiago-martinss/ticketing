d5427d81afa2127f36b15098efca335675cc6287
Thiago <martinsmthiago@gmail.com>
2025-04-28 19:44:04 -0300

    add listener.ts file

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,19 @@
+import nats from 'node-nats-streaming';
+
+console.clear();
+
+const stan = nats.connect('ticketing', '123', {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+  console.log('Listener connected to NATS');
+
+  const subscription = stan.subscribe('ticket:created');
+
+  subscription.on('message', (msg) => {
+    console.log('Message received');
+    const data = msg.getData();
+    console.log(data.toString('utf-8'));
+  })
+});

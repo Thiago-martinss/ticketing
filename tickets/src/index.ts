da1ab0113ef26335c1e12c792482dad8bfce1cb4
Thiago <martinsmthiago@gmail.com>
2025-05-04 23:20:42 -0300

    add ticket update publishing

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,14 +1,14 @@
-import { app } from "./app";
-import mongoose from "mongoose";
-import { natsWrapper } from "./nats-wrapper";
+import { app } from './app';
+import mongoose from 'mongoose';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
-    throw new Error("JWT_KEY must be defined");
+    throw new Error('JWT_KEY must be defined');
   }
 
   if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI must be defined");
+    throw new Error('MONGO_URI must be defined');
   }
 
   try {
@@ -16,16 +16,23 @@ const start = async () => {
       process.env.NATS_CLUSTER_ID!,
       process.env.NATS_CLIENT_ID!,
       process.env.NATS_URL!
-    )
+    );
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI);
 
-    console.log("Connected to MongoDb");
+    console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
   }
 
   app.listen(3000, () => {
-    console.log("Listening on port 3000!");
+    console.log('Listening on port 3000!');
   });
 };
 

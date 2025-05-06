97fae979f812bb990648b6de23f000b937d6d553
Thiago <martinsmthiago@gmail.com>
2025-05-05 23:17:12 -0300

    add order.ts and order-status file

---
+++ orders/src/models/order.ts
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose';
+import { OrderStatus } from '@tmatta-tickets/common';
+
+interface OrderAttrs {
+  userId: string;
+  expiresAt: Date;
+  status: OrderStatus;
+  ticket: TicketDoc;
+}
+
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  expiresAt: Date;
+  status: OrderStatus;
+  ticket: TicketDoc;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket',
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+
+interface TicketDoc extends mongoose.Document {
+  id: string;
+  title: string;
+  price: number;
+}
+
+export { Order };
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,24 @@
-import express, { Request, Response } from "express";
+import mongoose from 'mongoose';
+import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@tmatta-tickets/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post("/api/orders", (req: Request, res: Response) => {
-  res.send([]);
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId is required'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send([]);
+  }
+);
 
 export { router as newOrderRouter };

67909605cdbb1f4fde58a1ead28e82f5ff693ba8
Thiago <martinsmthiago@gmail.com>
2025-05-08 13:31:44 -0300

    add delete order route

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,32 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
+import { Order, OrderStatus } from '../models/order';
+import {
+  requireAuth,
+  NotFoundError,
+  NotAuthorizedError,
+} from '@tmatta-tickets/common';
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", (req: Request, res: Response) => {
-  res.send([]);
-});
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate('ticket');
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.send(order);
+  }
+);
 
 export { router as deleteOrderRouter };

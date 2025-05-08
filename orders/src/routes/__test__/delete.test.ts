68a3c4f5caf178e73f1fce1fd72a2243c6013731
Thiago <martinsmthiago@gmail.com>
2025-05-08 16:55:51 -0300

    add event publishing test

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 import { Order, OrderStatus } from '../../models/order';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('marks an order as cancelled', async () => {
   // Create a ticket
@@ -26,4 +27,27 @@ it('marks an order as cancelled', async () => {
     .expect(204);
   const updatedOrder = await Order.findById(order.id);
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it('emits an order cancelled event', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  const userOne = global.signin();
+  // Make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', userOne)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+  // Make a request to delete the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', userOne)
+    .send()
+    .expect(204);
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -5,6 +5,8 @@ import {
   NotFoundError,
   NotAuthorizedError,
 } from '@tmatta-tickets/common';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -24,6 +26,13 @@ router.delete(
 
     order.status = OrderStatus.Cancelled;
     await order.save();
+
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
 
     res.send(order);
   }

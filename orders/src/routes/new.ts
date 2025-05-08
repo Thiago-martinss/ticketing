f274c0bc9a2ba42dddee52959477796b937f9b03
Thiago <martinsmthiago@gmail.com>
2025-05-08 11:39:30 -0300

    add order tests

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
 import {
-  NotFoundError,
   requireAuth,
   validateRequest,
-  BadRequestError,
+  NotFoundError,
   OrderStatus,
+  BadRequestError,
 } from '@tmatta-tickets/common';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
@@ -23,25 +23,29 @@ router.post(
       .not()
       .isEmpty()
       .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
-      .withMessage('TicketId is required'),
+      .withMessage('TicketId must be provided'),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
     const { ticketId } = req.body;
 
+    // Find the ticket the user is trying to order in the database
     const ticket = await Ticket.findById(ticketId);
     if (!ticket) {
       throw new NotFoundError();
     }
 
+    // Make sure that this ticket is not already reserved
     const isReserved = await ticket.isReserved();
     if (isReserved) {
       throw new BadRequestError('Ticket is already reserved');
     }
 
+    // Calculate an expiration date for this order
     const expiration = new Date();
     expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
+    // Build the order and save it to the database
     const order = Order.build({
       userId: req.currentUser!.id,
       status: OrderStatus.Created,
@@ -49,6 +53,8 @@ router.post(
       ticket,
     });
     await order.save();
+
+    // Publish an event saying that an order was created
 
     res.status(201).send(order);
   }
--- orders/src/test/setup.ts
+++ orders/src/test/setup.ts
@@ -1,20 +1,18 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import { app } from '../app';
-import request from 'supertest';
 import jwt from 'jsonwebtoken';
 
 declare global {
   var signin: () => string[];
 }
-
 jest.mock('../nats-wrapper');
 
-let mongo: MongoMemoryServer;
-
+let mongo: any;
 beforeAll(async () => {
-  process.env.JWT_KEY = 'my-token';
-  mongo = await MongoMemoryServer.create();
+  process.env.JWT_KEY = 'asdfasdf';
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+  const mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
 
   await mongoose.connect(mongoUri, {});
@@ -32,22 +30,22 @@ beforeEach(async () => {
 
 afterAll(async () => {
   await mongoose.connection.close();
-
   if (mongo) {
     await mongo.stop();
   }
 });
 
 global.signin = () => {
+  // Build a JWT payload.  { id, email }
   const payload = {
     id: new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com',
   };
 
-  // Create the JWT
+  // Create the JWT!
   const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  // Build session Object. { jwt: MY_JWT}
+  // Build session Object. { jwt: MY_JWT }
   const session = { jwt: token };
 
   // Turn that session into JSON
@@ -56,7 +54,6 @@ global.signin = () => {
   // Take JSON and encode it as base64
   const base64 = Buffer.from(sessionJSON).toString('base64');
 
-  // Return the cookie string with the encoded data
+  // return a string thats the cookie with the encoded data
   return [`session=${base64}`];
 };
-

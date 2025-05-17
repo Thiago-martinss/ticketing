506e2465ebeec3b20c86b4e3d1f7a9831289b112
Thiago <martinsmthiago@gmail.com>
2025-05-16 21:26:44 -0300

    add order-created-listener test

--- common/src/events/order-created-event.ts
+++ common/src/events/order-created-event.ts
@@ -1,11 +1,12 @@
-import { Subjects } from "./subjects";
+import { Subjects } from './subjects';
+import { OrderStatus } from './types/order-status';
 
 export interface OrderCreatedEvent {
   subject: Subjects.OrderCreated;
   data: {
     id: string;
     version: number;
-    status: string;
+    status: OrderStatus;
     userId: string;
     expiresAt: string;
     ticket: {

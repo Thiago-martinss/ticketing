d876dad9199d8b73cf19f028113f9df6a1d61115
Thiago <martinsmthiago@gmail.com>
2025-05-16 22:06:57 -0300

    add tests

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -1,29 +1,31 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import { app } from '../app';
 import request from 'supertest';
+import { app } from '../app';
 import jwt from 'jsonwebtoken';
 
 declare global {
-  var signin: () => string[];
+  var signin: (id?: string) => string[];
 }
 
 jest.mock('../nats-wrapper');
 
-let mongo: MongoMemoryServer;
-
+let mongo: any;
 beforeAll(async () => {
-  process.env.JWT_KEY = 'my-token';
-  mongo = await MongoMemoryServer.create();
+  process.env.JWT_KEY = 'asdfasdf';
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+  const mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
 
   await mongoose.connect(mongoUri, {});
 });
 
 beforeEach(async () => {
+  jest.clearAllMocks();
   if (mongoose.connection.db) {
     const collections = await mongoose.connection.db.collections();
- 
+
     for (let collection of collections) {
       await collection.deleteMany({});
     }
@@ -31,23 +33,23 @@ beforeEach(async () => {
 });
 
 afterAll(async () => {
-  await mongoose.connection.close();
-
   if (mongo) {
     await mongo.stop();
   }
+  await mongoose.connection.close();
 });
 
-global.signin = () => {
+global.signin = (id?: string) => {
+  // Build a JWT payload.  { id, email }
   const payload = {
-    id: new mongoose.Types.ObjectId().toHexString(),
+    id: id || new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com',
   };
 
-  // Create the JWT
+  // Create the JWT!
   const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  // Build session Object. { jwt: MY_JWT}
+  // Build session Object. { jwt: MY_JWT }
   const session = { jwt: token };
 
   // Turn that session into JSON
@@ -56,7 +58,6 @@ global.signin = () => {
   // Take JSON and encode it as base64
   const base64 = Buffer.from(sessionJSON).toString('base64');
 
-  // Return the cookie string with the encoded data
+  // return a string thats the cookie with the encoded data
   return [`session=${base64}`];
 };
-
